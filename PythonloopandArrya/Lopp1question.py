a9f8782fef5cd1a6e1ff0a524e6ae1e70896f66e
aru447 <arusharmacs@gmail.com>
2025-12-01 12:56:35 +0530

    code added

---
+++ PythonloopandArrya/Lopp1question.py
@@ -0,0 +1,29 @@
+#2. Print all even numbers from 1 to N.
+# count = 1 
+# Number =100;
+# for index in range(count,Number):
+#     if index%2==0:
+#         print(index)
+
+#3. Print all numbers divisible by 3 from 1 to N.
+# Count  =  1
+# number =  100
+# for index in range(Count,number):
+#     if(index%3==0):
+#         print(index)
+
+#4. Count digits in a number without converting to a string.
+# countNum  =  1233
+# num  = 0
+# # for _ in range(100):
+# #     if countNum >0:
+# #         countNum =  countNum//10
+# #         num =  num +1
+        
+# # print(num)
+# #Using while loop
+# while countNum>0:
+#     countNum  =  countNum //10
+#     num  =  num +1
+# print(num)
+
